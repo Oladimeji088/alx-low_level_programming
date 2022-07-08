1389816c88701c4921a6104dcec19dffba24bbf9
Rufai Abdulwasii Oladimeji <rufaiwasiu5@gmail.com>
2022-07-07 21:15:24 -0700

    hope

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -20,7 +19,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else
+	else if (c >= a && c >= b)
 	{
 		largest = c;
 	}
